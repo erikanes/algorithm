50a100e668769f832df0fb90614c4b5abceeb533
erikanes <znzkzk112@gmail.com>
2023-02-24 11:26:30 +0900

    1 solved

---
+++ BOJ/S2_24479.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool visited[100001];
+vector<int> arr[100001];
+int ans[100001];
+
+void dfs(const int& adj) {
+    if (visited[adj]) return;
+    
+    static int idx = 1;
+    ans[adj] = idx++;
+    visited[adj] = true;
+
+    for (const int& i : arr[adj])
+        dfs(i);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n, m, r;
+    cin >> n >> m >> r;
+
+    while (m--) {
+        int u, v;
+        cin >> u >> v;
+        arr[u].push_back(v);
+        arr[v].push_back(u);
+    }
+    
+    for (int i = 1; i <= n; ++i)
+        sort(arr[i].begin(), arr[i].end(), less<>());
+
+    dfs(r);
+
+    for (int i = 1; i <= n; ++i)
+        cout << ans[i] << '\n';
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -92,3 +92,4 @@
 |9655|돌 게임|<img align=center width=15 src=https://d2gd6pc034wcta.cloudfront.net/tier/6.svg>|230219|
 |5525|IOIOI|<img align=center width=15 src=https://d2gd6pc034wcta.cloudfront.net/tier/10.svg>|230221|
 |5639|이진 검색 트리|<img align=center width=15 src=https://d2gd6pc034wcta.cloudfront.net/tier/11.svg>|230223|
+|24479|알고리즘 수업 - 깊이 우선 탐색 1|<img align=center width=15 src=https://d2gd6pc034wcta.cloudfront.net/tier/9.svg>|230224|
